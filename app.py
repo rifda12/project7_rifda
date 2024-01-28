7ee150b87c32bcfafa00cfb9dfee315c49e4da11
M. Asadullah Al Ghozi <rifdatulhusnaa@gmail.com>
2024-01-28 17:08:28 +0700

    commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+import pandas as pd
+from modules.insurance_model import InsuranceModel
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to the API modeling Prefiction Insurance!"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    df = pd.DataFrame(data)
+    result_predict = InsuranceModel().runModel(df, typed='single')
+    return jsonify({
+        "status": "predicted",
+        "predicted_result": result_predict
+    })
+
+if __name__ == "__main__":
+    app.run(port=8000)
